4cb6e55af463bb80163b835cb41a6db290bdd842
mahirpatel04 <patel.mahir.h@gmail.com>
2024-04-28 03:13:53 -0700

    mistake

--- main.py
+++ main.py
@@ -12,23 +12,24 @@ from moviepy.video.tools.subtitles import SubtitlesClip
 link = "https://www.youtube.com/watch?v=952ILTHDgC4"
 ttsFile = "test.txt"
 
-# Check if background video is already downloaded or not
-try:
-    vid = downloadVideo(link, "test.mp4")
-except FileExistsError:
-    print("Vid already existed, so did not re-download")
-    vid = Video("test.mp4", 720)
+def main(link, ttsFile):
+    # Check if background video is already downloaded or not
+    try:
+        vid = downloadVideo(link, "test.mp4")
+    except FileExistsError:
+        print("Vid already existed, so did not re-download")
+        vid = Video("test.mp4", 720)
 
-# Check if TTS is already created or not
-try:
-    tts = createTTS(ttsFile, "test.mp3")
-except FileExistsError:
-    print("TTS already existed, so did not re-download")
-    tts = Audio(ttsFile)
+    # Check if TTS is already created or not
+    try:
+        tts = createTTS(ttsFile, "test.mp3")
+    except FileExistsError:
+        print("TTS already existed, so did not re-download")
+        tts = Audio(ttsFile)
 
 
-# Check if Base Video is already created or not
-try:
-    createBaseVideo(tts.fileName, vid.fileName, "testOutput.mp4", vid.res)
-except FileExistsError:
-    print("Final video already exists, new one NOT CREATED")
+    # Check if Base Video is already created or not
+    try:
+        createBaseVideo(tts.fileName, vid.fileName, "testOutput.mp4", vid.res)
+    except FileExistsError:
+        print("Final video already exists, new one NOT CREATED")
